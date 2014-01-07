a606e4bcda6eb92b88d3ba0e7412d3530ae9381a
tubaxenor <tubaxenor@gmail.com>
2014-01-07 23:41:30 +0800

    Add batsd check

---
+++ files/default/plugins/check-check_and_close.rb
@@ -0,0 +1,18 @@
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-plugin/check/cli'
+require 'batsd'
+
+class CheckCheckAndClose < Sensu::Plugin::Check::CLI
+
+  def run
+    total = error_check(Time.now - 720)
+    critical if total < config[:crit]
+    warning  if total < config[:warn]
+    ok
+  end
+
+  def error_check(t2, t1 = Time.now)
+    b = Batsd.new(:host => '127.0.0.1')
+    b.values('counters:check_and_close_exception', t2, t1).reduce(:+)
+  end
+end
